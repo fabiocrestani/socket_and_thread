66b61401f2ab790a1ba006510e3b4771d812d018
Fabio Crestani <crestani.fabio@gmail.com>
2020-03-01 15:07:54 +0100

    Add conditional mutex into buffer

--- circular_buffer.c
+++ circular_buffer.c
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+
 #include "circular_buffer.h"
 
 void buffer_init(CircularBuffer *buf, size_t element_size)
@@ -15,18 +17,26 @@ void buffer_init(CircularBuffer *buf, size_t element_size)
     buf->idx_cons = 0;
 	buf->element_size = element_size;
 	buf->data = malloc(BUFFER_SIZE * element_size);
+
+	buf->ready_flag = 0;
+	pthread_mutex_init(&(buf->thread_ready_mutex), NULL);
+	pthread_cond_init(&(buf->thread_ready_cond), NULL);
 }
 
 void buffer_denit(CircularBuffer *buf)
 {
 	free(buf->data);
+
+	pthread_mutex_destroy(&(buf->thread_ready_mutex));
+	pthread_cond_destroy(&(buf->thread_ready_cond));
 }
 
 void buffer_put(CircularBuffer *buf, void *new_element)
 {
 	if (buf->element_counter < BUFFER_SIZE)
 	{
-		void *target = (char *) buf->data + (buf->element_size * buf->idx_prod++);
+		void *target = (int *) buf->data + (buf->element_size * buf->idx_prod);
+		buf->idx_prod++;
 		memcpy(target, new_element, buf->element_size);
 		buf->element_counter++;
 		if (buf->idx_prod >= BUFFER_SIZE)
@@ -40,9 +50,10 @@ uint8_t buffer_pop(CircularBuffer *buf, void *target)
 {
 	if (buf->element_counter)
 	{
-		void *source = (char*) buf->data + (buf->element_counter * buf->element_size);
+		void *source = (int *) buf->data + (buf->element_size * buf->idx_cons);
 		buf->element_counter--;
 		memcpy(target, source, buf->element_size);
+		buf->idx_cons++;
 		if (buf->idx_cons >= BUFFER_SIZE)
 		{
 			buf->idx_cons = 0;
@@ -62,11 +73,30 @@ uint8_t buffer_is_empty(CircularBuffer *buf)
 	return isEmpty;
 }
 
+// Reference: 
+// https://stackoverflow.com/questions/2551529/block-until-an-event-has-completed
 uint8_t buffer_has_room_for(CircularBuffer *buf, uint32_t len)
 {
 	return ((buf->element_counter + len) < BUFFER_SIZE);
 }
 
+void buffer_wait_ready(CircularBuffer *buf)
+{
+	pthread_mutex_lock(&(buf->thread_ready_mutex));
+	while (!buf->ready_flag)
+	{
+    	pthread_cond_wait(&(buf->thread_ready_cond), &(buf->thread_ready_mutex));
+	}
+	pthread_mutex_unlock(&(buf->thread_ready_mutex));
+}
 
 
+void buffer_set_ready(CircularBuffer *buf, int ready)
+{
+	pthread_mutex_lock(&(buf->thread_ready_mutex));
+	buf->ready_flag = ready;
+
+	pthread_cond_signal(&(buf->thread_ready_cond)); // or using pthread_cond_broadcast(&ready_cond);
+	pthread_mutex_unlock(&(buf->thread_ready_mutex));
+}
 
--- circular_buffer.h
+++ circular_buffer.h
@@ -7,23 +7,33 @@
 #define __CIRCULARBUFFER_H__
 
 #include <stdint.h>
+#include <pthread.h>
 
 #define BUFFER_SIZE 1024*2
 
 #define DEBUG_CIRCULAR_BUFFER
 
 typedef struct _circularBuffer {
-	uint16_t    idx_prod;
-	uint16_t    idx_cons;
-	uint16_t    element_counter;
-	void     	*data;
-	size_t 		element_size;
+	uint16_t idx_prod;
+	uint16_t idx_cons;
+	uint16_t element_counter;
+	void *data;
+	size_t element_size;
+
+	int ready_flag;
+	pthread_mutex_t thread_ready_mutex;
+	pthread_cond_t thread_ready_cond;
+
 } CircularBuffer;
 
 void buffer_init(CircularBuffer *buf, size_t element_size);
+void buffer_denit(CircularBuffer *buf);
 void buffer_put(CircularBuffer *buf, void *new_element);
 uint8_t buffer_pop(CircularBuffer *buf, void *target);
 uint8_t buffer_is_empty(CircularBuffer *buf);
 uint8_t buffer_has_room_for(CircularBuffer *buf, uint32_t len);
+
+void buffer_wait_ready(CircularBuffer *buf);
+void buffer_set_ready(CircularBuffer *buf, int ready);
 
 #endif
